769850040aaccc5f6d8c1e739a94dc0c084b267f
Brandon <brandonmowat11@gmail.com>
2015-09-14 17:02:10 -0400

    broke it, working on making it a module and have 0 dependencies (depends on jQuery right now)

--- drippple.js
+++ drippple.js
@@ -1,39 +1,44 @@
-$(document).ready(function($){
-	// code by Brandon Mowat
-	// http://brandonmowat.com
-	// http://medium.com/@irl
-	// please leave the credits,  thx bruh
-	var xmlhttp = new XMLHttpRequest();
-	//########## HEY YOU! ##########
-	//######### YEAH YOU! ##########
-	// In this url variable, see where it says "/dchen"?
-	// replace that with your own dribbble username!
-	var url = "https://api.dribbble.com/v1/users/dchen/shots/?access_token=4b14b95f60442720be19906baf0bb1dad6eaa55aa38f88e4212c84ab91f37751";
-	var imgurls = [];
+(function(exports) {
+		// code by Brandon Mowat
+		// http://brandonmowat.com
+		// http://medium.com/@irl
+		// please leave the credits,  thx bruh
+		var xmlhttp = new XMLHttpRequest();
+		//########## HEY YOU! ##########
+		//######### YEAH YOU! ##########
+		// In this url variable, see where it says "/dchen"?
+		// replace that with your own dribbble username!
+		var url = "https://api.dribbble.com/v1/users/mowat/shots/?access_token=4b14b95f60442720be19906baf0bb1dad6eaa55aa38f88e4212c84ab91f37751";
+		var imgurls = [];
 
-	xmlhttp.onreadystatechange = function() {
-	    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-	        var myArr = JSON.parse(xmlhttp.responseText);
-	        var newarr = getURLs(myArr);
-	        fill(imgurls);
-	    }
-	}
-
-	xmlhttp.open("GET", url, true);
-	xmlhttp.send();
-
-	function getURLs(arr) {
-		for(i = 0; i < arr.length; i++) {
-			imgurls.push(arr[i]['images']['hidpi']);
+		xmlhttp.onreadystatechange = function() {
+		    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+		        var myArr = JSON.parse(xmlhttp.responseText);
+		        var newarr = getURLs(myArr);
+		        fill(imgurls);
+		    }
 		}
-		console.log(imgurls);
-	}
 
-	function fill(arr) {
-	    var i = 0;
-	    $('.drip').each(function() {
-	    	$(this).attr('src',arr[i]);
-	    	i++;
-	    });
-	}
-});
+		xmlhttp.open("GET", url, true);
+		xmlhttp.send();
+
+		function getURLs(arr) {
+			for(i = 0; i < arr.length; i++) {
+				imgurls.push(arr[i]['images']['hidpi']);
+			}
+			console.log(imgurls);
+		}
+
+		// rewrite to not rely on jQuery
+		//
+		// function fill(arr) {
+		//     var i = 0;
+		//     $('.drip').each(function() {
+		//     	$(this).attr('src',arr[i]);
+		//     	i++;
+		//     });
+		// }
+
+	module.exports = drippple;
+
+})(this);
